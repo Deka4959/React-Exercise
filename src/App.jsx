47145670f15dfdef53dd261d328988a968cc89d9
Deka <suheeypcade143@gmail.com>
2026-07-26 17:04:29 +0200

    Ex 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import ProductCard from "./ProductCard";
+import { useEffect , useState } from "react";
+
+ 
 
 function App() {
-  return (
-    <>
-      <ProductCard />
-    </>
-  );
+const [name , setName]= useState("");
+const [salaan , setSalaan]= useState("assalama aleiukum");
+useEffect(()=>{
+
+  document.title= `${name} : ${salaan}`;
+
+  console.log(`${name}, ${salaan}`)
+
+
+}, [name , salaan]);
+
+  
+
+
+
+
+  return(
+    <>   
+    <h2>Enter your Name</h2>
+    <input type="text" value={name} onChange={(e)=> setName(e.target.value)}/>
+    <h2>Greating</h2>
+     <input type="text" value={salaan} onChange={(e)=> setSalaan(e.target.value)}/>
+  
+
+
+</>
+
+  ) ;
+  
 }
 
 export default App;
