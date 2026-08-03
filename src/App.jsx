e1c5925aa9b469709868de84e7047134d2756145
Deka <suheeypcade143@gmail.com>
2026-08-03 17:36:47 +0200

    ex 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,17 @@
-import { useState } from "react";
+import UserList from "./UserList";
 
 function App() {
-  const [user, setUser] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [search, setSearch] = useState("");
-
-  const getData = async () => {
-    try {
-      setLoading(true);
-
-      const response = await fetch(
-        `https://api.github.com/users/${search}`
-      );
-
-      const data = await response.json();
-      
-
-      setUser(data);
-    } 
-    catch (error) {
-      console.log(error);
-    }
-    finally {
-      setLoading(false);
-    }
-  };
-
+ 
+   
   return (
-    <>
-      <h1>Github User Search</h1>
 
-      <input
-        type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
+  <UserList />
+  
 
-      <button onClick={getData}>Search</button>
 
-      {loading && <p>Loading...</p>}
+  
+  
 
-      <p>{user.login}</p>
-      <p>{user.followers}</p>
-        <p>{user.public_repos}</p>
-        <p>{user.bio}</p>
-        <p>{user.avatar_url}</p>
-      
-    </>
   );
 }
 
