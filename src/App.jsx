06edfbc05f4bb6996c776aea3a1cf8bbf889d14a
Deka <suheeypcade143@gmail.com>
2026-08-06 20:43:17 +0200

    ex 13

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,55 @@
-import Counter from "./Counter";
+
+import { useReducer } from "react";
+
+function reducer(state ,action){
+if(action === "increment"){
+  return state + 1;
+
+}
+if(action === "decrement"){
+  return state - 1
+
+}
+if( action ==="Reset"){
+return 0;
+}
+
+
+
+return state;
+}
 
 function App() {
+  const  [count , dispatch] =useReducer(reducer, 0)
+    const  [countb , setCount] =useReducer(reducer, 0)
   return (
     <>
-      <Counter />
+    <h1>counter</h1>
+    <p>{count}</p>
+    <h1>counter A</h1>
+    <button onClick={()=> dispatch("increment")}>
+      A</button>
+      <button onClick={()=> dispatch("decrement")}>
+        A-
+      </button>
+
+<h1>Counter B</h1>
+       <p>{countb}</p>
+      
+      <button onClick={()=> setCount("increment")}>
+      B</button>
+      <button onClick={()=> setCount("decrement")}>
+        B-
+      </button>
+      <button
+  onClick={() => {
+    dispatch("Reset");
+    setCount("Reset");
+  }}
+>
+  Reset Both
+</button>
+      
     </>
   );
 }
