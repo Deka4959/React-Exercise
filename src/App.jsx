7ea21200da48c6a63bf704750e9e89b74c398332
Deka <suheeypcade143@gmail.com>
2026-08-12 21:29:14 +0200

    ex 14

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,155 @@
-
 import { useReducer } from "react";
 
-function reducer(state ,action){
-if(action === "increment"){
-  return state + 1;
+const initialState = {
+  page: 1,
+  name: "",
+  lastName: "",
+  email: "",
+  number: "",
+};
 
-}
-if(action === "decrement"){
-  return state - 1
+function reducer(state, action) {
+  switch (action.type) {
+    case "SET_NAME":
+      return {
+        ...state,
+        name: action.payload,
+      };
 
-}
-if( action ==="Reset"){
-return 0;
-}
+    case "SET_LASTNAME":
+      return {
+        ...state,
+        lastName: action.payload,
+      };
 
+    case "SET_EMAIL":
+      return {
+        ...state,
+        email: action.payload,
+      };
 
+    case "SET_NUMBER":
+      return {
+        ...state,
+        number: action.payload,
+      };
 
-return state;
+    case "NEXT":
+      return {
+        ...state,
+        page: state.page + 1,
+      };
+
+    case "BACK":
+      return {
+        ...state,
+        page: state.page - 1,
+      };
+
+    default:
+      return state;
+  }
 }
 
 function App() {
-  const  [count , dispatch] =useReducer(reducer, 0)
-    const  [countb , setCount] =useReducer(reducer, 0)
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <>
-    <h1>counter</h1>
-    <p>{count}</p>
-    <h1>counter A</h1>
-    <button onClick={()=> dispatch("increment")}>
-      A</button>
-      <button onClick={()=> dispatch("decrement")}>
-        A-
-      </button>
+      <h1>Multi Step Form</h1>
 
-<h1>Counter B</h1>
-       <p>{countb}</p>
-      
-      <button onClick={()=> setCount("increment")}>
-      B</button>
-      <button onClick={()=> setCount("decrement")}>
-        B-
-      </button>
-      <button
-  onClick={() => {
-    dispatch("Reset");
-    setCount("Reset");
-  }}
->
-  Reset Both
-</button>
-      
+    
+      {state.page === 1 && (
+        <div>
+          <h2>Page 1</h2>
+
+          <label>Name</label>
+          <input
+            type="text"
+            value={state.name}
+            onChange={(e) =>
+              dispatch({
+                type: "SET_NAME",
+                payload: e.target.value,
+              })
+            }
+          />
+
+          <br />
+
+          <label>Last Name</label>
+          <input type="text"
+            value={state.lastName}
+            onChange={(e) =>
+              dispatch({
+                type: "SET_LASTNAME",
+                payload: e.target.value,
+              })
+            }
+          />
+
+          <br />
+
+          <button onClick={() => dispatch({ type: "NEXT" })}>
+            Next
+          </button>
+        </div>
+      )}
+
+    
+      {state.page === 2 && (
+        <div>
+          <h2>Page 2</h2>
+
+          <label>Email</label>
+          <input type="email"
+            value={state.email}
+            onChange={(e) =>
+              dispatch({
+                type: "SET_EMAIL",
+                payload: e.target.value,
+              })
+            }
+          />
+
+          <br />
+
+          <label>Number</label>
+          <input type="number"
+            value={state.number}
+            onChange={(e) =>
+              dispatch({
+                type: "SET_NUMBER",
+                payload: e.target.value,
+              })
+            }
+          />
+
+          <br />
+
+          <button onClick={() => dispatch({ type: "BACK" })}>
+            Back
+          </button>
+
+          <button onClick={() => dispatch({ type: "NEXT" })}>
+            Next
+          </button>
+        </div>
+      )}
+      {state.page === 3 && (
+        <div>
+          <h2>All Information</h2>
+
+          <p>Name: {state.name}</p>
+          <p>Last Name: {state.lastName}</p>
+          <p>Email: {state.email}</p>
+          <p>Number: {state.number}</p>
+
+          <button onClick={() => dispatch({ type: "BACK" })}>
+            Back
+          </button>
+        </div>
+      )}
     </>
   );
 }
