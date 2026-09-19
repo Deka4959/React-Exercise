f95065139760bb524556abcb39efb22505e7ab4e
Deka <suheeypcade143@gmail.com>
2026-09-19 15:30:25 +0200

    ex 22

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,53 @@
-import DesignTailwind from "./DesignTailwind";
+import { useState } from "react";
+
 
 function App() {
-  return <DesignTailwind />;
+
+  const [formData, setFormData] =useState({
+    userName:"",
+    Email:"",
+    checkbox:""
+  })
+
+  const handleSubmit=(event)=>{
+    
+    event.preventDefault();
+    console.log("FormData")
+
+  }
+  const hanleChange=(event)=>{
+
+    const {name,value} = event.target
+
+    setFormData((prevData)=> ({...prevData , [name]: value}))
+   
+
+  }
+
+ 
+ 
+  
+
+return (
+ 
+<form onSubmit={handleSubmit}>
+<input type="text"name="userName" value={formData.userName} onChange={hanleChange} placeholder="enter uour username"/>
+
+<input type="text"name="Email" value={formData.Email} onChange={hanleChange} placeholder="enter your email"/>
+
+<input type="checkbox" name="checkbox"  value={formData.checkbox} onChange={hanleChange}/>
+
+<button type="submit" className="bg-amber-200 p-6 rounded-2xl">Submit</button>
+
+</form>
+
+ 
+ 
+  
+
+)
+
 }
+
 
 export default App;
